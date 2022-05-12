8e3b0ab58ab556bbf1047ecdece5c3c37fa6f5ac
Tom <thomasdhkim91@gmail.com>
2022-05-12 02:42:38 -0400

    added tests for pages and menu

--- app/transactions/__init__.py
+++ app/transactions/__init__.py
@@ -6,24 +6,31 @@ from flask import Blueprint, render_template, abort, url_for, current_app
 from flask_login import current_user, login_required
 from jinja2 import TemplateNotFound
 
+from app.auth import admin_required
 from app.db import db
 from app.db.models import Transactions
 from app.transactions.forms import csv_upload
 from werkzeug.utils import secure_filename, redirect
 
 transactions = Blueprint('transactions', __name__,
-                        template_folder='templates')
+                         template_folder='templates')
 
 
 @transactions.route('/transactions', methods=['GET'], defaults={"page": 1})
 @transactions.route('/transactions/<int:page>', methods=['GET'])
+@login_required
+@admin_required
 def transactions_browse(page):
     page = page
     per_page = 1000
     pagination = Transactions.query.paginate(page, per_page, error_out=False)
     data = pagination.items
+    add_url = 'trans_mgmt.add_transaction'
+    delete_url = ('trans_mgmt.delete_transaction', [('trans_id', ':id')])
+    edit_url = ('trans_mgmt.edit_transaction', [('trans_id', ':id')])
     try:
-        return render_template('browse_transactions.html',data=data,pagination=pagination)
+        return render_template('browse_trnx.html', data=data, pagination=pagination, add_url=add_url,
+                               delete_url=delete_url, edit_url=edit_url, Transactions=Transactions)
     except TemplateNotFound:
         abort(404)
 
@@ -38,17 +45,16 @@ def transactions_upload():
         filename = secure_filename(form.file.data.filename)
         filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
         form.file.data.save(filepath)
-        #user = current_user
+        # user = current_user
         list_of_transactions = []
-        #calculate the balance for the user
+        # calculate the balance for the user
         balance = 0
         with open(filepath) as file:
             csv_file = csv.DictReader(file)
             for row in csv_file:
                 log.info(row['AMOUNT'])
                 balance += int(row['AMOUNT'])
-                list_of_transactions.append(Transactions(row['AMOUNT'],row['TYPE']))
-
+                list_of_transactions.append(Transactions(row['AMOUNT'], row['TYPE']))
 
         current_user.transactions = list_of_transactions
         current_user.balance = balance
---
+++ tests/auth_test.py
@@ -0,0 +1,39 @@
+"""This test the homepage"""
+from app import db
+from app.db.models import User, Transactions
+
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
+
+def test_auth_pages(client):
+    """This makes the index page"""
+    response = client.get("/dashboard")
+    assert response.status_code == 302
+    response = client.get("/register")
+    assert response.status_code == 200
+    response = client.get("/login")
+    assert response.status_code == 200
+
+def test_login(application, client):
+    """Test that a user login functionality works"""
+    with application.app_context():
+        #Add user to be able to test login
+        user = User('test@test.com', 'testtest')
+        db.session.add(user)
+        db.session.commit()
+
+        res = client.post('/login', data=dict(email="test@test.com", password='testtest'), follow_redirects=True)
+        assert res.status_code == 200
+        assert b"Welcome" in res.data
+
+        #Test that the user can navigate to the dashboard and that it displays the user
+        dres = client.get("/dashboard", follow_redirects=True)
+        assert b"test" in dres.data
+
+        db.session.delete(user)
+
+
